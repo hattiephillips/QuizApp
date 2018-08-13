3e2d3701d654d1b8c89aaa771ec6e89243e9b7e9
hattiephillips <harriet-phillips@hotmail.co.uk>
2018-08-13 07:29:24 -0400

    development of proximity, addded get distance and tested, resources used refrenced in header

--- ucfahmp/www/js/proximity.js
+++ ucfahmp/www/js/proximity.js
@@ -14,3 +14,21 @@ function qProximity(any_array) {
 		}
 	}
 }
+
+function getDistanceFromLatLngInM(lat1,lon1,lat2,lon2) {
+	var R =6371;//Radius of earth
+	var dLat = deg2rad(lat2-lat1); //deg 2 red below
+	var dLon = deg2rad(lon2-lon1);
+	var a=
+	Math.sin(dlat/2) * Math.sin(dlat/2) +
+	Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+	Math.sin(dLon/2) * Math.sin(dLon/2)
+	;
+	var c=2 *Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+	var d=(R * c)*1000 //Distance in KM
+	return d;
+}
+
+function deg2rad(deg) {
+	return deg * (Math.PI/180)
+}
